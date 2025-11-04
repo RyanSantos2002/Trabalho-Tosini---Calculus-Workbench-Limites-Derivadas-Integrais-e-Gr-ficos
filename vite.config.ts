2b94d722acd6aca844937246866d50ba09306327
Ryan Matheus Dos Santos Nunes <ryan@MacBook-Air-de-Ryan.local>
2025-11-03 21:41:58 -0300

    fix: ensure correct lib path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // 🔥 aqui está a correção
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
